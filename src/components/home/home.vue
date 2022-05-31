70602abc0978aeb8a3eab3bec47dab87c631b6b2
CarolLau13 <825827342@qq.com>
2022-05-31 23:02:34 +0800

    v1

---
+++ src/components/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="home">
+    <button @click="getData">开始</button>
+  </div>
+</template>
+
+<script>
+import { getMultidata } from "network/home.js";
+export default {
+  name: "Home",
+  methods: {
+    getData() {},
+  },
+};
+</script>
+
+<style scoped>
+button {
+  display: block;
+  margin: 0 auto;
+}
+</style>
